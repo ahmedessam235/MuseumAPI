f7c13f727889e319714b39d03dc7e8012adb41f1
ahmedessam235 <ahmedessameltantawy@gmail.com>
2021-06-28 14:29:19 +0200

    adding image handling comment

--- Backend/src/routes/artRoutes.ts
+++ Backend/src/routes/artRoutes.ts
@@ -4,6 +4,7 @@ const artController = require('../controllers/artController');
 const userAuthentication = require('../middleware/userAuthentication');
 const staffAuthentication = require('../middleware/staffAuthentication');
 let app = express.Router()
+/* for simplicity the images will be hosted on Imgur if there is time TO- do ADD file and upload handling */
 app.get('/art', [userAuthentication.authenticateUser,artController.getArt]);
 app.post('/art', [staffAuthentication.authenticateAdmin,artController.addArt]);
 app.put('/art',  [staffAuthentication.authenticateAdmin,artController.updateArt]);

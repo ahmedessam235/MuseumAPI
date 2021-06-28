a73a76d0bc1355c95b37c9ed44ae59fc1ed3a658
ahmedessam235 <ahmedessameltantawy@gmail.com>
2021-06-28 20:15:31 +0200

    creating basic react app with folder structure and adding background image for login

---
+++ Frontend/museum-app/src/Components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Login(){
+
+
+
+
+    
+    return (
+        <div>
+            <h1>hello please log on in this form</h1>
+        </div>
+    );
+}
+
+export default Login;
